e4c6a96ebbfe7827472c22b07552a1ce10fb5afa
tendonintendo <bendoninbendo@gmail.com>
2024-12-15 15:29:24 +0700

    Change Filenames and Early Logic Implementation

---
+++ src/Logic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FP
+{
+    public class Logic
+    {
+        public static int countChanges = 0;
+        private List<Ruangan> _rooms;
+        private List<Benda> _changeables;
+        public Logic(List<Ruangan> rooms) 
+        {
+            _rooms = rooms;
+            _changeables = new List<Benda>();
+            foreach (Ruangan room in _rooms)
+            {
+                foreach (var item in room.Items)
+                {
+                    if (item.ImagePath.Contains("_awal"))
+                    {
+                        _changeables.Add(item);
+                    }
+                }
+            }
+        }
+
+        public void Transform()
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(0, _changeables.Count);
+            _changeables[num].ImagePath = _changeables[num].ImagePath.Replace("awal", "akhir");
+            countChanges++;
+        }
+    }
+}
