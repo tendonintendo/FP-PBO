3815d1d8d0e3eb94be00ecddc7dfc0f72708c007
tamamfb <tamamfajarbriliansyah@gmail.com>
2024-12-15 17:30:30 +0700

    tes

--- src/obj/Debug/net8.0-windows/FP.AssemblyInfo.cs
+++ src/obj/Debug/net8.0-windows/FP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88d20b5e1c4490241d405ea4328893b2385616f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb018c1b67aa4bcc516d683d3940bef5d0d4a640")]
 [assembly: System.Reflection.AssemblyProductAttribute("FP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
